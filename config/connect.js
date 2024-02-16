1bce062b8b6d5d0e0a93ffd2de3ee5497e30ffde
shahina42 <141030593+shahina42@users.noreply.github.com>
2024-02-16 11:50:24 +0530

    updated version

--- config/connect.js
+++ config/connect.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 
 const uri = process.env.MONGO_URI;
 
-const connectDb = async () => {
+const connectDb = async (req, res) => {
     try {
         await mongoose.connect(uri);
         console.log("Database connected successfully");
     } catch (error) {
         console.error("Error connecting to database:", error);
+        return res.status(500).send("Error connecting to database");
     }
 };
 
